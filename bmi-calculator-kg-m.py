e5da366f182bb5256b3625009042260e1bc7bdfb
Colleen <cmlohr@outlook.com>
2020-11-05 12:29:05 -0600

    updated bmi calculator and added odd-or-even program

--- bmi-calculator-kg-m.py
+++ bmi-calculator-kg-m.py
@@ -1,5 +1,30 @@
+print("################################")
+print("#####    BMI CALCULATOR    #####")
+print("################################")
+print("#####   Coded by @Cmlohr   #####")
+print("################################")
+print("                                ")
+
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
-bmi = int(weight) / float(height)**2
-bmi_int = int(bmi)
-print(bmi_int)
+
+## Calculation
+bmi = int(weight) / float(height) ** 2
+
+## Conversion to float
+bmi_float = float(bmi)
+
+## Formating to the second decimal place
+nbmi = "{:.2f}".format(bmi_float)
+
+## Conditions for BMI
+if bmi_float >= 35:
+  print(f"{nbmi}"+" Clinically Obese")
+elif bmi_float > 30:
+  print(f"{nbmi}"+" Obese")
+elif bmi_float > 25:
+  print(f"{nbmi}"+" Overweight")
+elif bmi_float >= 18.5:
+  print(f"{nbmi}"+" Healthy Weight")
+else:
+  print(f"{nbmi}"+" Underweight")
---
+++ odd-or-even.py
@@ -0,0 +1,9 @@
+print("########################")
+print("##### ODD OR EVEN  #####")
+print("########################")
+number = int(input("Which number do you want to check? \n"))
+
+if number % 2:
+  print("This is an odd number.")
+else:
+  print("This is an even number")
